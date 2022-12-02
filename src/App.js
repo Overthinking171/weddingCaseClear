f1b6dd34c00b10e84b848d22e620b80a6b553278
Overthinking171 <dadybima171@gmail.com>
2022-12-02 08:02:52 +0700

    first update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import TypeList from "./components/TypeList";
 // import Home from './pages/Home'
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Navbar/>}/>
+      </Routes>
+      <Routes>
+        <Route path="/" element={<TypeList/>} />
       </Routes>
     </BrowserRouter>
   );
